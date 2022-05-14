9dbe2f9183aecf70dd93ccb34d12aeedac12d748
AllenWong2002 <applewang03@gmail.com>
2022-05-15 01:14:34 +0800

    version 1.0.0

--- fileIO.c
+++ fileIO.c
@@ -5,7 +5,7 @@
 typedef struct data{
     char name[21];
     char gender;
-    char hobby[5][21]; 
+    char hobby[5][21];
     char phone_number[11];
     char area[16];
     char target;
@@ -13,7 +13,7 @@ typedef struct data{
     float height;
     char zodiac[21];
     char income[11];
-    char work[21];
+    char job[41];
     char self_introduction[151];
 } Data;
 
@@ -23,17 +23,17 @@ int main(){
     int data_amount = 0;
     Data person[780];
     data_amount = read_file(person);
-    //printf("%d", data_amount); //看讀到的人數對不對
+    printf("%d", data_amount);                      // 看讀到的人數對不對
 }
 
 int read_file(Data *person){
-    int data_id = 0;
-    const char *filename = "test.txt";
+    int data_id = 0;                                // 檔名要記得改自己txt的名字喔
+    const char *filename = "allen_bise.txt";
     FILE *input_file = fopen(filename, "r");
     if (!input_file){
         exit(EXIT_FAILURE);
     }
-    while (fscanf(input_file, "%s %c %s %s %s %s %s %s %s %c %d %f %s %s %s\n"
+    while (fscanf(input_file, "%s %c %s %s %s %s %s %s %s %c %d %f %s %s %s "
     , person[data_id].name
     , &person[data_id].gender
     , person[data_id].hobby[0]
@@ -48,12 +48,12 @@ int read_file(Data *person){
     , &person[data_id].height
     , person[data_id].zodiac
     , person[data_id].income
-    , person[data_id].work) != EOF){
+    , person[data_id].job) != EOF){
         fgets(person[data_id].self_introduction, 151, input_file);
         data_id++;
     }
     fclose(input_file);
-    /*int temp_i = data_id;         //這個只是看他有沒有讀進去 要測試有沒有讀進去的話把註解弄掉
+    /*int temp_i = data_id;                       //這個只是看他有沒有讀進去 要測試有沒有讀對的話把註解弄掉
     for (int data_id = 0; data_id < temp_i; data_id++){
         printf("%s %c %s %s %s %s %s %s %s %c %d %.1f %s %s %s\n%s\n"
         , person[data_id].name
@@ -70,8 +70,8 @@ int read_file(Data *person){
         , person[data_id].height
         , person[data_id].zodiac
         , person[data_id].income
-        , person[data_id].work
+        , person[data_id].job
         , person[data_id].self_introduction);
-    }*/ 
+    }*/
     return data_id;
 }
